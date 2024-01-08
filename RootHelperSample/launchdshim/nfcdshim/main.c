284f7d112e4203d48e8e3aeb2381fc64b1401879
Ljzd-PRO <63289359+Ljzd-PRO@users.noreply.github.com>
2024-01-09 07:57:11 +0800

    refactor: set nfcdshim/main.c return 0

--- RootHelperSample/launchdshim/nfcdshim/main.c
+++ RootHelperSample/launchdshim/nfcdshim/main.c
@@ -7,8 +7,6 @@
 int (*SBSystemAppMain)(int argc, char *argv[], char *envp[], char* apple[]);
 
 int main(int argc, char *argv[], char *envp[], char* apple[]) {
-    void *handle = dlopen("/System/Library/PrivateFrameworks/SpringBoard.framework/SpringBoard", RTLD_GLOBAL);
-    SBSystemAppMain = dlsym(handle, "SBSystemAppMain");
-    return SBSystemAppMain(argc, argv, envp, apple);
+    return 0;
 }
 
